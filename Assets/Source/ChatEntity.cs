6a3605d9eba88711d92b958d1affd931b9c55116
Sacristan <girts.kesteris@gmail.com>
2016-05-22 14:28:51 +0300

    Initial Commit

---
+++ Assets/Source/ChatEntity.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(NetworkIdentity))]
+public class ChatEntity : NetworkBehaviour
+{
+    string message;
+    int wordLimit=50;
+
+    private NetworkIdentity networkIdentity;
+    
+    void Start()
+    {
+        networkIdentity = GetComponent<NetworkIdentity>();
+    }
+
+    [Command]
+    void CmdSendMessageToServer(string words)
+    {
+        RpcSentMessageToClients(words);
+    }
+
+    [ClientRpc]
+    void RpcSentMessageToClients(string words)
+    {
+        ChatManager.PushMessage(words);
+    }
+
+    void OnGUI()
+    {
+        if (isLocalPlayer)
+        {
+            GUILayout.Space(150);
+            message = GUILayout.TextArea(message, wordLimit);
+            if (GUILayout.Button("Send"))
+            {
+                if (networkIdentity.isServer)
+                {
+                    RpcSentMessageToClients(message);
+                }
+                else
+                {
+                    CmdSendMessageToServer(message);
+                }
+            }
+        }
+    }
+
+}
---
+++ Assets/Source/ChatManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(UnityEngine.Networking.NetworkManager))]
+public class ChatManager : MonoBehaviour
+{
+    private static ChatManager instance;
+
+    private List<string> messages = new List<string>();
+    [SerializeField]
+    private int messageArrayLimit = 10;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public static void PushMessage(string message)
+    {
+        instance.messages.Add(message);
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Space(200);
+        GUILayout.Box("MessageList");
+
+        foreach (string message in messages)
+        {
+            GUILayout.Box(message);
+        }
+    }
+}
